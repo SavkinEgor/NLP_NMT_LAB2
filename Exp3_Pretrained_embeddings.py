38144405a20d8c9335a7a677fa1ce8e0ab066f55
SavkinEgor <46623732+SavkinEgor@users.noreply.github.com>
2021-06-20 15:41:56 +0300

    added supporting files for experiments

---
+++ Exp3_Pretrained_embeddings.py
@@ -0,0 +1,179 @@
+import random
+
+from nltk.tokenize import WordPunctTokenizer
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchtext.legacy.data import Field
+
+MAX_LENGTH = 100
+device = "cpu"
+PAD_IDX = 1
+
+tokenizer_W = WordPunctTokenizer()
+
+
+def tokenize(x, tokenizer=tokenizer_W):
+    return tokenizer.tokenize(x.lower())
+
+
+SRC = Field(tokenize=tokenize,
+            init_token='<sos>',
+            eos_token='<eos>',
+            lower=True)
+
+TRG = Field(tokenize=tokenize,
+            init_token='<sos>',
+            eos_token='<eos>',
+            lower=True)
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout, bidirectional=False):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.emb_dim = emb_dim
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+        weight = torch.FloatTensor(SRC.vocab.vectors)
+        self.embedding = nn.Embedding.from_pretrained(weight)
+
+        self.rnn = nn.LSTM(
+            input_size=emb_dim,
+            hidden_size=hid_dim,
+            num_layers=n_layers,
+            bidirectional=bidirectional,
+            dropout=dropout
+        )
+
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, src):
+
+        inp = torch.ones(MAX_LENGTH, src.shape[1], dtype=torch.long, device=device) * PAD_IDX
+        inp[:src.shape[0], :] = src
+        embedded = self.embedding(inp)
+
+        embedded = self.dropout(embedded)
+
+        output, (hidden, cell) = self.rnn(embedded)
+
+        return output, hidden, cell
+
+    def init_weights(self):
+        initrange = 0.1
+        self.rnn.bias_ih_l0.data.zero_()
+        self.rnn.weight_ih_l0.data.uniform_(-initrange, initrange)
+        self.rnn.bias_hh_l0.data.zero_()
+        self.rnn.weight_hh_l0.data.uniform_(-initrange, initrange)
+
+
+class DecoderAttn(nn.Module):
+    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout, bidirectional=False, max_length=MAX_LENGTH):
+        super(DecoderAttn, self).__init__()
+
+        self.emb_dim = emb_dim
+        self.hid_dim = hid_dim
+        self.output_dim = output_dim
+        self.n_layers = n_layers
+        self.dropout = dropout
+        self.bidirectional = bidirectional
+        self.max_length = max_length
+
+        weight = torch.FloatTensor(TRG.vocab.vectors)
+        self.embedding = nn.Embedding.from_pretrained(weight)
+
+        self.rnn = nn.LSTM(
+            input_size=emb_dim,
+            hidden_size=hid_dim,
+            num_layers=n_layers,
+            dropout=dropout,
+            bidirectional=bidirectional
+        )
+        multiplier = 2 if bidirectional else 1
+        self.attn = nn.Linear(self.hid_dim + self.emb_dim, self.max_length)
+        self.attn_combine = nn.Linear(self.hid_dim * multiplier + self.emb_dim, self.emb_dim)
+
+        self.out = nn.Linear(
+            in_features=hid_dim * multiplier,
+            out_features=output_dim
+        )
+
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, input, hidden, cell, encoder_outputs):
+
+        input = input.unsqueeze(0)
+        embedded = self.dropout(self.embedding(input))
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        # print(attn_weights.shape, encoder_outputs.shape, embedded.shape)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0).permute(1, 0, 2),
+                                 encoder_outputs.permute(1, 0, 2)
+                                 ).permute(1, 0, 2)
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        # print(output.shape, self.attn_combine)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+
+        output, (hidden, cell) = self.rnn(output, (hidden, cell))
+        # print(output.shape, self.out)
+        prediction = self.out(output.squeeze(0))
+
+        return prediction, hidden, cell, attn_weights
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+    def init_weights(self):
+        initrange = 0.1
+        self.rnn.bias_ih_l0.data.zero_()
+        self.rnn.weight_ih_l0.data.uniform_(-initrange, initrange)
+        self.rnn.bias_hh_l0.data.zero_()
+        self.rnn.weight_hh_l0.data.uniform_(-initrange, initrange)
+        self.attn.bias.data.zero_()
+        self.attn.weight.data.uniform_(-initrange, initrange)
+        self.attn_combine.bias.data.zero_()
+        self.attn_combine.weight.data.uniform_(-initrange, initrange)
+        self.out.bias.data.zero_()
+        self.out.weight.data.uniform_(-initrange, initrange)
+
+
+class Seq2SeqAttn(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+        assert encoder.hid_dim == decoder.hid_dim, \
+            "Hidden dimensions of encoder and decoder must be equal!"
+        assert encoder.n_layers == decoder.n_layers, \
+            "Encoder and decoder must have equal number of layers!"
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        batch_size = trg.shape[1]
+        max_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+
+        outputs = torch.zeros(max_len, batch_size, trg_vocab_size).to(self.device)
+
+        encoder_outputs, hidden, cell = self.encoder(src)
+
+        input = trg[0, :]
+
+        for t in range(1, max_len):
+            output, hidden, cell, attn_weights = self.decoder(input, hidden, cell, encoder_outputs)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.max(1)[1]
+            input = (trg[t] if teacher_force else top1)
+
+        return outputs
---
+++ Exp4_Attention_concat.py
@@ -0,0 +1,121 @@
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+MAX_LENGTH = 100
+device = "cpu"
+PAD_IDX = 1
+
+
+class EncoderAttn(nn.Module):
+    def __init__(self, input_dim, emb_dim, enc_hid_dim, dec_hid_dim, n_layers, dropout):
+        super().__init__()
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+        lstm_dropout = dropout if n_layers > 1 else 0
+        self.rnn = nn.LSTM(emb_dim, enc_hid_dim, n_layers, bidirectional=True, dropout=lstm_dropout)
+        self.fc_h = nn.Linear(enc_hid_dim * 2 * n_layers, dec_hid_dim)
+        self.fc_c = nn.Linear(enc_hid_dim * 2 * n_layers, dec_hid_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src):
+        embedded = self.dropout(self.embedding(src))
+        outputs, (hidden, cell) = self.rnn(embedded)
+        hidden = torch.tanh(
+            self.fc_h(torch.cat((hidden[-2, :, :],
+                                 hidden[-1, :, :],
+                                 hidden[-3, :, :],
+                                 hidden[-4, :, :]),
+                                dim=1)))
+        cell = torch.tanh(self.fc_c(torch.cat((cell[-2, :, :],
+                                               cell[-1, :, :],
+                                               cell[-3, :, :],
+                                               cell[-4, :, :]),
+                                              dim=1)))
+        return outputs, hidden, cell.unsqueeze(0)
+
+
+class Attention(nn.Module):
+    def __init__(self, enc_hid_dim, dec_hid_dim):
+        super().__init__()
+        self.attn = nn.Linear((enc_hid_dim * 2) + dec_hid_dim, dec_hid_dim)
+        self.v = nn.Linear(dec_hid_dim, 1, bias=False)
+
+    def forward(self, hidden, encoder_outputs):
+        src_len = encoder_outputs.shape[0]
+        #         print(hidden.shape, encoder_outputs.shape, self.attn)
+        hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        #         print(hidden.shape, encoder_outputs.shape, self.attn, self.v)
+        energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim=2)))
+        attention = self.v(energy).squeeze(2)
+        return F.softmax(attention, dim=1)
+
+
+class DecoderAttn(nn.Module):
+    def __init__(self, output_dim, emb_dim, enc_hid_dim, dec_hid_dim, n_layers, dropout, attention):
+        super().__init__()
+
+        self.output_dim = output_dim
+        self.attention = attention
+        self.dec_hid_dim = dec_hid_dim
+        self.n_layers = n_layers
+        lstm_dropout = dropout if n_layers > 1 else 0
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+
+        #         self.fc_h = nn.Linear(dec_hid_dim, dec_hid_dim * n_layers)
+
+        self.rnn = nn.LSTM((enc_hid_dim * 2) + emb_dim, dec_hid_dim, 1, dropout=lstm_dropout)
+        self.fc_out = nn.Linear((enc_hid_dim * 2) + dec_hid_dim + emb_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, input, hidden, cell, encoder_outputs):
+        input = input.unsqueeze(0)
+        embedded = self.dropout(self.embedding(input))
+        #         print(hidden.shape, encoder_outputs.shape, self.attention, "\n\n")
+        a = self.attention(hidden, encoder_outputs)
+        a = a.unsqueeze(1)
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        weighted = torch.bmm(a, encoder_outputs)
+
+        weighted = weighted.permute(1, 0, 2)
+        rnn_input = torch.cat((embedded, weighted), dim=2)
+        #         hidden = self.fc_h(hidden)
+        #         hidden = hidden.reshape(self.n_layers, BATCH_SIZE, self.dec_hid_dim)
+        #         cell = cell.reshape(self.n_layers, BATCH_SIZE, self.dec_hid_dim)
+        output, (hidden, cell) = self.rnn(rnn_input, (hidden.unsqueeze(0), cell))
+        #         assert (output == hidden).all()
+
+        embedded = embedded.squeeze(0)
+        output = output.squeeze(0)
+        weighted = weighted.squeeze(0)
+
+        prediction = self.fc_out(torch.cat((output, weighted, embedded), dim=1))
+        return prediction, hidden.squeeze(0), cell
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        batch_size = src.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
+        encoder_outputs, hidden, cell = self.encoder(src)
+        input = trg[0, :]
+
+        for t in range(1, trg_len):
+            output, hidden, cell = self.decoder(input, hidden, cell, encoder_outputs)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.argmax(1)
+            input = trg[t] if teacher_force else top1
+
+        return outputs
